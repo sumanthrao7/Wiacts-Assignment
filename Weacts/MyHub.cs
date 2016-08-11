2fcf4eb380bc3386f23b82a0b4f990c3734f55f5
sumanthrao7 <sumanthrao7@gmail.com>
2016-08-11 07:07:08 -0600

    Initial Commit

---
+++ Weacts/MyHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Weacts
+{
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            //to delay 5 seconds
+            System.Threading.Thread.Sleep(5000);
+            string userName = "";
+            if(HttpContext.Current != null && HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                userName = HttpContext.Current.User.Identity.Name;
+            }
+            Clients.All.showEmail("Hello! Your email id: " + userName);
+        }
+    }
+}
